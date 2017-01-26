cfa931f259466c9ec4e4cba11159eddcadb004be
John McDowall <john@mcdowall.info>
2017-01-25 18:15:15 -0800

    Newline the log entries

--- app/Modules/Dashboard/components/Dashboard.js
+++ app/Modules/Dashboard/components/Dashboard.js
@@ -9,7 +9,7 @@ class Dashboard extends Component {
     return (
       <div className={Styles.Dashboard}>
         <h1>Dashboard</h1>
-        <textarea value={this.props.logs}>
+        <textarea value={this.props.logs.join('\n')}>
         </textarea>
       </div>
     )
