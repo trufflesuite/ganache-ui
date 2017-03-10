eea93a2de8d0037c85535ed38fabd6bc697a94d8
John McDowall <john@mcdowall.info>
2017-03-09 18:26:59 -0800

    Getting ready for Snapshots

--- app/Modules/Snapshots/components/Snapshots.js
+++ app/Modules/Snapshots/components/Snapshots.js
@@ -1,10 +1,16 @@
-
 import React, { Component } from 'react'
+import Styles from './Snapshots.css'
 
 export default class Snapshots extends Component {
   render () {
     return (
-      <h1>SNAPSHOTS</h1>
+      <div className={Styles.Snapshots}>
+        <h4>SNAPSHOTS</h4>
+        <main>
+        </main>
+        <footer>
+        </footer>
+      </div>
     )
   }
 }
