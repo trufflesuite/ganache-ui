d4eb9e606841a97515d7c5a28a95781f8f250ed7
John McDowall <john@mcdowall.info>
2017-06-29 15:41:01 -0700

    Transactions

--- app/Modules/Blocks/components/Blocks.js
+++ app/Modules/Blocks/components/Blocks.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import RecentBlocks from './blocks/RecentBlocks'
-import RecentTxs from './txs/RecentTxs'
 
 import Styles from './Blocks.css'
 
@@ -11,10 +10,6 @@ export default class Blocks extends Component {
         <RecentBlocks
           blocks={this.props.testRpcState.blocks}
           appSearchBlock={this.props.appSearchBlock}
-          appSearchTx={this.props.appSearchTx}
-          testRpcState={this.props.testRpcState}
-        />
-        <RecentTxs
           appSearchTx={this.props.appSearchTx}
           testRpcState={this.props.testRpcState}
         />
