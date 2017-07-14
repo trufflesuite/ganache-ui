8a52a8d9f0cbbcdf71b284575fd0974491708e4a
John McDowall <john@mcdowall.info>
2017-07-14 15:11:01 -0700

    Fixup svg fill color

--- app/Elements/Spinner.js
+++ app/Elements/Spinner.js
@@ -3,7 +3,7 @@ import React, { PureComponent } from 'react'
 class Spinner extends PureComponent {
   render () {
     return (
-        <svg width={this.props.width} height={this.props.height} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" className="uil-ring-alt">
+        <svg width={this.props.width} height={this.props.height} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" className="isolated uil-ring-alt">
           <rect x="0" y="0" width="100" height="100" fill="none" className="bk"></rect>
           <circle cx="50" cy="50" r="40" stroke="rgba(29, 31, 46, 1)" fill="none" strokeWidth="10" strokeLinecap="round"></circle>
           <circle cx="50" cy="50" r="40" stroke="rgba(230, 167, 99, 1.000)" fill="none" strokeWidth="6" strokeLinecap="round">
