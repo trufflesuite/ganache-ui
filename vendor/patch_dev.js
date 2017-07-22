cf6a468cb065c839a5d7302eb01691b4c96c0759
John McDowall <john@mcdowall.info>
2017-07-22 12:48:15 -0700

    Monkey patch the local Electron Info.plist to make it look like Ganache is running, not an Electron app in development mode

---
+++ vendor/patch_dev.js
@@ -0,0 +1,32 @@
+const fs = require('fs-extra')
+const path = require('path')
+const plist = require('plist')
+const rcedit = require('rcedit')
+
+const packageJSON = require('../package.json')
+
+if (process.platform === 'darwin') {
+  console.log('Monkey patching your local Electron Info.plist to make it look like Ganache')
+  // Patch Info.plist values
+  const plistPath = path.resolve(
+    __dirname,
+    '..',
+    'node_modules/electron/dist/Electron.app/Contents/Info.plist'
+  )
+  const plistContent = plist.parse(fs.readFileSync(plistPath, 'utf8'))
+
+  plistContent.CFBundleDisplayName = packageJSON.productName
+  plistContent.CFBundleExecutable = packageJSON.productName
+  plistContent.CFBundleIdentifier = `com.dev.${packageJSON.name}`
+  plistContent.CFBundleName = packageJSON.productName
+  plistContent.CFBundleVersion = packageJSON.version
+  plistContent.CFBundleShortVersionString = packageJSON.version
+
+  fs.writeFileSync(plistPath, plist.build(plistContent))
+} else if (process.platform === 'win32') {
+  console.log('Monkey patching your electron.exe icon')
+  const icon = path.resolve(__dirname, '../resources/icons/win/icon.ico')
+  rcedit(require('electron'), {
+    icon
+  }, () => {})
+}
