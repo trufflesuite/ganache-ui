872e9a170b41918bf37dfe658091933833a63433
Mike Seese <seesemichaelj@gmail.com>
2018-06-12 15:19:29 -0400

    incorporate GA reporting for error handling

--- src/Kernel/ErrorHandler.js
+++ src/Kernel/ErrorHandler.js
@@ -1,6 +1,20 @@
 
 import * as Core from '../Actions/Core'
 import * as Settings from '../Actions/Settings'
+import {GoogleAnalytics as GA} from '../Services/GoogleAnalytics'
+
+function reportError(action, label) {
+  let event = {
+    category: "error",
+    action
+  }
+
+  if (label) {
+    event.label = label
+  }
+
+  GA.reportEvent(event)
+}
 
 export function handleError(store, error) {
   let showModal = false
@@ -12,22 +26,31 @@ export function handleError(store, error) {
       case "EACESS":
         store.dispatch(Settings.setSettingError("server.port", "The port is used by another application; please change it"))
         activeConfigTab = "server"
+        reportError("network", error.code)
         break
       case "EADDRNOTAVAIL":
         store.dispatch(Settings.setSettingError("server.hostname", "The hostname is not local address; only use hostnames/IPs associated with this machine"))
         activeConfigTab = "server"
+        reportError("network", error.code)
         break
       case "CUSTOMERROR":
         store.dispatch(Settings.setSettingError(error.key, error.value))
         activeConfigTab = error.tab
+        reportError("custom", error.key + " - " + error.value)
+        break
+      case "CHAINEXIT":
+        showModal = true
+        reportError("chain-exit", error.exitCode + " - " + error.exitSignal + (error.logError ? (" - " + error.logError) : ""))
         break
       default:
         showModal = true
+        reportError("generic", error.code)
         break
     }
   }
   else {
     showModal = true
+    reportError("generic", JSON.stringify(error))
   }
 
   store.dispatch(Core.setSystemError(error, showModal))
