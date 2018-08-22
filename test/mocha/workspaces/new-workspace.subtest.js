043e0e750f6c4f8f683aecee92b1c3efee1d2961
Mike Seese <seesemichaelj@gmail.com>
2018-08-22 12:49:20 -0400

    implement new workspace test

---
+++ test/mocha/workspaces/new-workspace.subtest.js
@@ -0,0 +1,33 @@
+
+import Workspace from "../../../src/main/types/workspaces/Workspace";
+import temp from "temp";
+import assert from "assert";
+import fs from "fs";
+import path from "path";
+import isEqual from "lodash.isequal";
+
+describe("New Workspace", () => {
+  let configDirectory = "/";
+  let workspace;
+
+  before("create folder where workspace will live", async () => {
+    temp.track();
+    configDirectory = temp.mkdirSync("ganache-temp-workspaces");
+    fs.mkdirSync(path.join(configDirectory, "workspaces"));
+    console.log(configDirectory);
+  });
+
+  it("successfully creates and bootsraps new workspace", async () => {
+    workspace = new Workspace("Temp Workspace", configDirectory);
+    workspace.bootstrap();
+  });
+
+  it("created expected settings files", async () => {
+    assert(fs.existsSync(workspace.workspaceDirectory), "Workspace directory wasn't created");
+    const settingsFile = path.join(workspace.workspaceDirectory, "Settings");
+    assert(fs.existsSync(settingsFile), "Workspace Settings file wasn't created");
+    const settings = JSON.parse(fs.readFileSync(settingsFile));
+    const workspaceSettings = workspace.settings._getAllRaw();
+    assert(isEqual(workspaceSettings, settings), "The settings in the workspace don't match what's in the Settings file");
+  });
+});
