c7753decfb9679ac2c6771795dd143ec5fc34960
Adrian Li <li.adrianmc@gmail.com>
2018-09-16 13:34:41 -0700

    add contract card to transaction page

--- src/renderer/screens/transactions/TxCard.js
+++ src/renderer/screens/transactions/TxCard.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react'
-import { hashHistory } from 'react-router'
+import React, { Component } from "react"
+import { hashHistory } from "react-router"
 
-import connect from '../helpers/connect'
+import connect from "../helpers/connect"
 
-import * as Transactions from '../../../common/redux/transactions/actions'
+import * as Transactions from "../../../common/redux/transactions/actions"
 
-import DestinationAddress from './DestinationAddress'
-import TransactionTypeBadge from './TransactionTypeBadge'
+import DestinationAddress from "./DestinationAddress"
+import TransactionTypeBadge from "./TransactionTypeBadge"
 
-import FormattedEtherValue from '../../components/formatted-ether-value/FormattedEtherValue'
+import FormattedEtherValue from "../../components/formatted-ether-value/FormattedEtherValue"
 
 class TxCard extends Component {
-  componentDidMount () {
-    this.props.dispatch(Transactions.showTransaction(this.props.transactionHash))
+  componentDidMount() {
+    this.props.dispatch(
+      Transactions.showTransaction(this.props.transactionHash)
+    )
   }
 
-  render () {
-    const tx = this.props.transactions.currentTransaction
-    const receipt = this.props.transactions.currentTransactionReceipt
+  render() {
+    const {
+      currentTransaction: tx,
+      currentTransactionReceipt: receipt
+    } = this.props.transactions
 
     if (!tx || !receipt) {
       return <div />
@@ -25,93 +29,114 @@ class TxCard extends Component {
 
     return (
       <section className="TxCard">
-        <header>
-          <button className="Button" onClick={hashHistory.goBack}>
-            &larr; Back
-          </button>
+        <div className="TxInfo">
+          <header>
+            <button className="Button" onClick={hashHistory.goBack}>
+              &larr; Back
+            </button>
 
-          <div className="Title">
-            <h1>
-              TX {tx.hash}
-            </h1>
-          </div>
-        </header>
-
-        <section className="Parties">
-          <div className="From">
-            <div className="Label">SENDER ADDRESS</div>
-            <div className="Value">
-              {tx.from}
+            <div className="Title">
+              <h1>TX {tx.hash}</h1>
             </div>
-          </div>
-          <DestinationAddress tx={tx} receipt={receipt} />
-          <div>
-            <div className="Value">
-              <div className="Type">
-                <TransactionTypeBadge tx={tx} receipt={receipt} />
+          </header>
+
+          <section className="Parties">
+            <div className="From">
+              <div className="Label">SENDER ADDRESS</div>
+              <div className="Value">{tx.from}</div>
+            </div>
+            <DestinationAddress tx={tx} receipt={receipt} />
+            <div>
+              <div className="Value">
+                <div className="Type">
+                  <TransactionTypeBadge tx={tx} receipt={receipt} />
+                </div>
               </div>
             </div>
-          </div>
+          </section>
 
-        </section>
+          <section className="Gas">
+            <div>
+              <div className="Label">VALUE</div>
+              <div className="Value">
+                <FormattedEtherValue value={tx.value.toString()} />
+              </div>
+            </div>
+            <div>
+              <div className="Label">GAS USED</div>
+              <div className="Value">{receipt.gasUsed}</div>
+            </div>
 
-        <section className="Gas">
-          <div>
-            <div className="Label">VALUE</div>
-            <div className="Value">
-              <FormattedEtherValue
-                value={tx.value.toString()}
-              />
+            <div>
+              <div className="Label">GAS PRICE</div>
+              <div className="Value">{tx.gasPrice.toString()}</div>
             </div>
-          </div>
-          <div>
-            <div className="Label">GAS USED</div>
-            <div className="Value">
-              {receipt.gasUsed}
-            </div>
-          </div>
 
-          <div>
-            <div className="Label">GAS PRICE</div>
-            <div className="Value">
-              {tx.gasPrice.toString()}
+            <div>
+              <div className="Label">GAS LIMIT</div>
+              <div className="Value">{tx.gas}</div>
             </div>
-          </div>
 
-          <div>
-            <div className="Label">GAS LIMIT</div>
-            <div className="Value">
-              {tx.gas}
-            </div>
-          </div>
+            {/* <div>
+              <div className="Label">MINED ON</div>
+              <div className="Value">
+                <Moment unix format="YYYY-MM-DD HH:mm:ss">
+                  {EtherUtil.bufferToInt(tx.block.header.timestamp)}
+                </Moment>
+              </div>
+            </div> */}
 
-          {/* <div>
-            <div className="Label">MINED ON</div>
-            <div className="Value">
-              <Moment unix format="YYYY-MM-DD HH:mm:ss">
-                {EtherUtil.bufferToInt(tx.block.header.timestamp)}
-              </Moment>
+            <div>
+              <div className="Label">MINED IN BLOCK</div>
+              <div className="Value">{receipt.blockNumber}</div>
             </div>
-          </div> */}
+          </section>
+          <main>
+            <div>
+              <div className="Label">TX DATA</div>
+              <div className="Value">{tx.input}</div>
+            </div>
+          </main>
+        </div>
 
-          <div>
-            <div className="Label">MINED IN BLOCK</div>
-            <div className="Value">
-              {receipt.blockNumber}
+        <div className="ContractInfo">
+          <header>
+            <div className="Title">
+              <h1>CONTRACT INFO</h1>
             </div>
-          </div>
-        </section>
-        <main>
-          <div>
-            <div className="Label">TX DATA</div>
-            <div className="Value">
-              {tx.input}
+          </header>
+
+          <section>
+            <div>
+              <div className="Label">NAME</div>
+              <div className="Value">ComplexTokenSent</div>
             </div>
-          </div>
-        </main>
+            <div>
+              <div className="Label">ADDRESS</div>
+              <div className="Value">0xPLACEHOLDER_ADDRESS_HERE</div>
+            </div>
+          </section>
+
+          <section>
+            <div>
+              <div className="Label">INPUTS</div>
+              <div className="Value">5, "Jim"</div>
+            </div>
+            <div>
+              <div className="Label">STATE CHANGES</div>
+              <div className="Value">
+                <div>ID: 3 >> 5</div>
+                <div>Name: "Jimothy" >> "Jim"</div>
+              </div>
+            </div>
+          </section>
+        </div>
       </section>
     )
   }
 }
 
-export default connect(TxCard, "transactions")
+export default connect(
+  TxCard,
+  "transactions"
+)
