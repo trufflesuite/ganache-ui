a7b8b590f629e1a0656f9fdd45e04b55b8d2990c
Adrian Li <li.adrianmc@gmail.com>
2018-09-23 15:28:20 -0400

    style encoded event details

--- src/renderer/screens/event-details/EventDetailsScreen.js
+++ src/renderer/screens/event-details/EventDetailsScreen.js
@@ -1,34 +1,44 @@
 import React from "react"
 import { hashHistory } from "react-router"
 
-const EncodedEventDetails = ({ txHash, blockTime, contractName }) => (
-  <div className="EncodedEventDetails">
-    <div className="Notice">
-      To see rich event data, link a Truffle Project containing the contract
-      that emits this event.
-    </div>
-    <div className="DataRow">
-      <div className="DataPoint">
-        <div className="Label">TX HASH</div>
-        <div className="Value">{txHash}</div>
+const EncodedEventDetails = ({ event }) => {
+  const { txHash, blockTime, contractName } = event
+  return (
+    <div className="EncodedEventDetails">
+      <div className="Notice">
+        <span className="Warning">⚠</span>{" "}
+        <strong>To see rich event data</strong> link a Truffle Project
+        containing the contract that emits this event.
       </div>
-      <div className="DataPoint">
-        <div className="Label">BLOCK TIME</div>
-        <div className="Value">{blockTime}</div>
+      <div className="DataRow">
+        <div className="DataPoint">
+          <div className="Label">TX HASH</div>
+          <div className="Value">{txHash}</div>
+        </div>
+        <div className="DataPoint">
+          <div className="Label">BLOCK TIME</div>
+          <div className="Value">{blockTime}</div>
+        </div>
+      </div>
+      <div className="DataRow">
+        <div className="DataPoint">
+          <div className="Label">CONTRACT</div>
+          <div className="Value">{contractName}</div>
+        </div>
       </div>
     </div>
-    <div className="DataRow">
-      <div className="DataPoint">
-        <div className="Label">CONTRACT</div>
-        <div className="Value">{contractName}</div>
-      </div>
-    </div>
-  </div>
-)
+  )
+}
 
 export default props => {
   const { logIndex, transactionHash } = props.routeParams
   console.log(logIndex, transactionHash)
+
+  const PLACEHOLDER_EVENT = {
+    txHash: "PLACEHOLDER_EVENT_TX_HASH",
+    blockTime: "PLACEHOLDER_BLOCKTIME",
+    contractName: "PLACEHOLDER_CONTRACT_NAME"
+  }
   return (
     <div className="EventDetails">
       <div className="TitleBar">
@@ -37,7 +47,7 @@ export default props => {
         </button>
         <h1 className="Title">{transactionHash}</h1>
       </div>
-      <EncodedEventDetails />
+      <EncodedEventDetails event={PLACEHOLDER_EVENT} />
     </div>
   )
 }
