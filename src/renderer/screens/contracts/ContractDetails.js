b879db5fda8b506a6998bdbafd75f7dd4f158dfd
Mike Seese <seesemichaelj@gmail.com>
2018-10-06 00:19:46 -0700

    update color for address

--- src/renderer/screens/contracts/ContractDetails.js
+++ src/renderer/screens/contracts/ContractDetails.js
@@ -97,7 +97,7 @@ class ContractDetails extends Component {
                   base00: 'rgba(0, 0, 0, 0)',
                   base01: 'rgb(245, 245, 245)',
                   base02: '#000', // lines and background color for: NULL, undefined, and brackets
-                  base03: '#93a1a1', // blue grey -- not used?
+                  base03: 'rgb(26, 185, 70)', // blue grey -- not used?
                   base04: 'rgba(0, 0, 0, 0.3)',
                   base05: '#aaa', // undefined text
                   base06: '#073642', // dark blue -- not sued?
@@ -109,7 +109,8 @@ class ContractDetails extends Component {
                   base0C: '#777', // array indexes and item counts
                   base0D: '#000', // arrows
                   base0E: '#000', // used for some arrows and bool
-                  base0F: '#268bd2' // a bright blue -- not used?
+                  base0F: '#268bd2', // a bright blue -- not used?
+                  base10: 'rgb(26, 185, 70)' // a bright blue -- not used?
               }}
               iconStyle="triangle"
               edit={false}
