dcf2d1dad8395c6f2aa868946427fe4318f6e380
HL <36248745+honestbonsai@users.noreply.github.com>
2019-01-23 13:21:44 -0500

    Fix improper export syntax

--- src/truffle-integration/projectFsWatcherUtils.js
+++ src/truffle-integration/projectFsWatcherUtils.js
@@ -1,9 +1,13 @@
 const path = require("path");
 
-export const getAncestorDirs = (truffle_directory, currDir, dirs = []) => {
+const getAncestorDirs = (truffle_directory, currDir, dirs = []) => {
   const parent = path.dirname(currDir);
 
   if (parent === currDir) return dirs;
   else if (parent === truffle_directory) return [parent, ...dirs];
   return getAncestorDirs(truffle_directory, parent, [parent, ...dirs]);
+};
+
+module.exports = {
+  getAncestorDirs,
 };
