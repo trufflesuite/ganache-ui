5242995009d2d5928949848c30fb37a74316e116
HL <36248745+honestbonsai@users.noreply.github.com>
2019-01-28 11:37:01 -0500

    Refactor truffle integration tests in jest

--- src/truffle-integration/__tests__/projectFsWatcherUtils.test.js
+++ src/truffle-integration/__tests__/projectFsWatcherUtils.test.js
@@ -1,38 +1,37 @@
-import assert from "assert";
 import { getAncestorDirs } from "../projectFsWatcherUtils";
 
 describe("projectFsWatcherUtils", () => {
   describe("getAncestorDirs", () => {
     const truffle_directory = "/path/to/awesome/project";
 
-    it("returns a list of all ancestor dirs up to truffle_directory when contracts_build_directory is a child of truffle_directory", () => {
+    test("returns a list of all ancestor dirs up to truffle_directory when contracts_build_directory is a child of truffle_directory", () => {
       const contracts_build_directory = `${truffle_directory}/contracts`;
       const dirs = getAncestorDirs(
         truffle_directory,
         contracts_build_directory,
       );
 
-      assert.deepEqual(dirs, [truffle_directory]);
+      expect(dirs).toEqual([truffle_directory]);
     });
 
-    it("returns a list of all ancestor dirs up to truffle_directory when contracts_build_directory is a descendant of truffle_directory", () => {
+    test("returns a list of all ancestor dirs up to truffle_directory when contracts_build_directory is a descendant of truffle_directory", () => {
       const contracts_build_directory = `${truffle_directory}/build/contracts`;
       const dirs = getAncestorDirs(
         truffle_directory,
         contracts_build_directory,
       );
 
-      assert.deepEqual(dirs, [truffle_directory, `${truffle_directory}/build`]);
+      expect(dirs).toEqual([truffle_directory, `${truffle_directory}/build`]);
     });
 
-    it("returns a list of all ancestor dirs when contracts_build_directory is not a descendent of truffle_directory", () => {
+    test("returns a list of all ancestor dirs when contracts_build_directory is not a descendent of truffle_directory", () => {
       const contracts_build_directory = `/path/to/build/contracts`;
       const dirs = getAncestorDirs(
         truffle_directory,
         contracts_build_directory,
       );
 
-      assert.deepEqual(dirs, ["/", "/path", "/path/to", "/path/to/build"]);
+      expect(dirs).toEqual(["/", "/path", "/path/to", "/path/to/build"]);
     });
   });
 });
