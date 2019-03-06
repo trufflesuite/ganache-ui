ff6948f5147c5b7a2229bf49e540e30fdf39dde4
HL <36248745+honestbonsai@users.noreply.github.com>
2019-03-06 17:18:42 -0500

    Split out redux connected part and UI part

--- src/renderer/screens/logs/LogContainerLazy.js
+++ src/renderer/screens/logs/LogContainerLazy.js
@@ -1,45 +1,7 @@
-import React, { Component } from "react";
-import { List, AutoSizer } from "react-virtualized";
+import LogsLazy from "./LogsLazy";
 import connect from "../helpers/connect";
-import Row from "./Row";
-
-class LogContainer extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    const isLineLengthDiff =
-      nextProps.logs.lines.length !== this.props.logs.lines.length;
-    const isListHeightDiff = nextState.listHeight !== this.state.listHeight;
-
-    return isLineLengthDiff || isListHeightDiff;
-  }
-
-  renderRow = ({ index, style }) => (
-    <Row index={index} style={style} log={this.props.logs.lines[index]} />
-  );
-
-  render() {
-    const { logs } = this.props;
-
-    return (
-      <div className="LogContainer" ref={this.LogContainer}>
-        <ul>
-          <AutoSizer>
-            {({ height, width }) => (
-              <List
-                width={width}
-                height={height}
-                rowCount={logs.lines.length}
-                rowHeight={35} // TODO needs to fit text, use variable size list?
-                rowRenderer={this.renderRow}
-              />
-            )}
-          </AutoSizer>
-        </ul>
-      </div>
-    );
-  }
-}
 
 export default connect(
-  LogContainer,
+  LogsLazy,
   "logs",
 );
