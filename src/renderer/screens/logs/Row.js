16c6fac34eaa9424a77f6f199784254f894f238b
HL <36248745+honestbonsai@users.noreply.github.com>
2019-03-07 10:53:28 -0500

    Add propTypes to Row

--- src/renderer/screens/logs/Row.js
+++ src/renderer/screens/logs/Row.js
@@ -1,9 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Row = ({ index, style, log }) => (
+const Row = ({ index, log, style }) => (
   <li key={index} className="plain" style={style}>
     {`[${new Date(log.time).toLocaleTimeString()}]`} {log.line}
   </li>
 );
+
+Row.propTypes = {
+  index: PropTypes.number,
+  log: PropTypes.shape({
+    time: PropTypes.Date.isRequired,
+    line: PropTypes.string.isRequired,
+  }).isRequired,
+  style: PropTypes.object, // required by react-virtualized
+};
 
 export default Row;
