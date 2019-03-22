bf35218a902715047e81bc9be1a6524052c355ba
Mike Seese <seesemichaelj@gmail.com>
2019-03-21 23:05:56 -0400

    add retry logic by manually finding the node path for nvm

--- src/truffle-integration/projectDetails.js
+++ src/truffle-integration/projectDetails.js
@@ -4,7 +4,46 @@ const child_process = require("child_process");
 const TruffleConfig = require("truffle-config");
 const temp = require("temp");
 
-async function get(projectFile) {
+const noNodeErrorMessage =
+  "Could not find 'node'. NodeJS is required to be installed to link Truffle projects.";
+
+async function attemptRetry(projectFile) {
+  const nvmDir = path.join(process.env.HOME, ".nvm");
+
+  if (fs.existsSync(nvmDir)) {
+    let shell;
+
+    if (fs.existsSync("/bin/bash")) {
+      shell = "/bin/bash";
+    } else if (fs.existsSync("/usr/bin/bash")) {
+      shell = "/usr/bin/bash";
+    } else {
+      throw new Error(noNodeErrorMessage);
+    }
+
+    process.env.NVM_DIR = nvmDir;
+    const nodeVersion = child_process
+      .execSync(
+        "source " +
+          path.join(nvmDir, "nvm.sh") +
+          " && nvm_resolve_local_alias default",
+        {
+          shell: shell,
+          encoding: "utf8",
+        },
+      )
+      .trim();
+
+    const nodeDir = path.join(nvmDir, "versions", "node", nodeVersion, "bin");
+    process.env.PATH = nodeDir + ":" + process.env.PATH;
+
+    return await get(projectFile, true);
+  } else {
+    throw new Error(noNodeErrorMessage);
+  }
+}
+
+async function get(projectFile, isRetry = false) {
   return new Promise(async (resolve, reject) => {
     try {
       const configFileDirectory = path.dirname(projectFile);
@@ -60,11 +99,14 @@ async function get(projectFile) {
         stdio: ["pipe", "pipe", "pipe", "ipc"],
       };
       const child = child_process.spawn("node", args, options);
-      child.on("error", error => {
+      child.on("error", async error => {
         if (error.code === "ENOENT") {
-          throw new Error(
-            "Could not find 'node'. NodeJS is required to be installed to link Truffle projects.",
-          );
+          // could not find node. check to see if they have ~/.nvm
+          if (!isRetry) {
+            resolve(await attemptRetry(projectFile));
+          } else {
+            throw new Error(noNodeErrorMessage);
+          }
         } else {
           throw new Error(error);
         }
