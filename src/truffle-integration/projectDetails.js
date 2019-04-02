0e69004094dbe1a1693446fe9c28b3262a7cf5dd
Mike Seese <seesemichaelj@gmail.com>
2019-04-02 18:55:01 -0400

    add retry logic by manually finding the node path for nvm (#1166)
    Fixes #1165 by loading the `~/.nvm/nvm.sh` script in a `bash` shell session to get the default node version. It then adds `~/.nvm/versions/node/${version}/bin` to `process.env.PATH` and tries to load the project again. It only retries once.

--- src/truffle-integration/projectDetails.js
+++ src/truffle-integration/projectDetails.js
@@ -3,8 +3,60 @@ const path = require("path");
 const child_process = require("child_process");
 const TruffleConfig = require("truffle-config");
 const temp = require("temp");
+const { promisify } = require("util");
+const exec = promisify(child_process.exec.bind(child_process));
 
-async function get(projectFile) {
+const noNodeErrorMessage =
+  "Could not find 'node'. Node.js is required to be installed to link Truffle projects.";
+
+async function getNodeVersionFromShell(shell, nvmDir) {
+  try {
+    const nvmPath = path.join(nvmDir, "nvm.sh");
+    const nodeVersion = await exec(
+      `unset npm_config_prefix && source ${nvmPath} && nvm_resolve_local_alias default`,
+      {
+        shell,
+        encoding: "utf8",
+      },
+    );
+    if (typeof nodeVersion === "string") {
+      return nodeVersion.trim();
+    }
+  } catch (e) {
+    // We throw the error away because we really only care if it worked or not
+  }
+  return null;
+}
+
+async function attemptRetry(projectFile) {
+  // Windows is SOL at this point. If people are installing node via nvm-windows
+  // (https://github.com/coreybutler/nvm-windows) and manage to ignore the path
+  // the same way linux does we can try to handle that here. For now, just bail.
+  if (process.platform === "win32") {
+    throw new Error(noNodeErrorMessage);
+  }
+  const nvmDir = path.join(process.env.HOME, ".nvm");
+  const shellLocations = ["/bin/bash", "/usr/bin/bash"];
+  // search shell locations for our node version
+  let nodeVersion = null;
+  for (let i = 0; i < shellLocations.length && nodeVersion == null; i++) {
+    nodeVersion = await getNodeVersionFromShell(shellLocations[i], nvmDir);
+  }
+
+  // if we still don't have a node version we're SOL
+  if (!nodeVersion) {
+    throw new Error(noNodeErrorMessage);
+  }
+
+  // now fix up the PATH to include our user's node dir
+  const nodeDir = path.join(nvmDir, "versions", "node", nodeVersion, "bin");
+  process.env.PATH = nodeDir + path.delimiter + (process.env.PATH || "");
+  process.env.NVM_DIR = nvmDir;
+
+  return await get(projectFile, true);
+}
+
+async function get(projectFile, isRetry = false) {
   return new Promise(async (resolve, reject) => {
     try {
       const configFileDirectory = path.dirname(projectFile);
@@ -60,14 +112,31 @@ async function get(projectFile) {
         stdio: ["pipe", "pipe", "pipe", "ipc"],
       };
       const child = child_process.spawn("node", args, options);
-      child.on("error", error => {
+      child.on("error", async error => {
+        const response = {
+          name: name,
+          configFile: projectFile,
+          config: {},
+          contracts: [],
+        };
         if (error.code === "ENOENT") {
-          throw new Error(
-            "Could not find 'node'. NodeJS is required to be installed to link Truffle projects.",
-          );
+          // could not find node. check to see if they have ~/.nvm
+          if (isRetry) {
+            response.error = error.message;
+          } else {
+            try {
+              resolve(await attemptRetry(projectFile));
+              return;
+            } catch (e) {
+              // Don't crash if we can't do anything about it. Just inform
+              // the user of the error
+              response.error = e.message;
+            }
+          }
         } else {
-          throw new Error(error);
+          response.error = error.message;
         }
+        resolve(response);
       });
       child.stderr.on("data", data => {
         throw new Error(data);
