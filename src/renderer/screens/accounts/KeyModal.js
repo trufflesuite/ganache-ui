a5763359be4a7c2375930c775788f7443ca2c41d
David Murdoch <davidmurdoch@users.noreply.github.com>
2019-08-26 20:26:25 -0400

    fix: add more security warnings (#1415)

--- src/renderer/screens/accounts/KeyModal.js
+++ src/renderer/screens/accounts/KeyModal.js
@@ -3,24 +3,31 @@ import React, { PureComponent } from "react";
 import ChecksumAddress from "../../components/checksum-addresses/ChecksumAddress";
 import Modal from "../../components/modal/Modal";
 
-import KeyIcon from "../../icons/key.svg";
-
 export default class KeyModal extends PureComponent {
   render() {
     return (
       <Modal className="KeyModal">
         <header>
-          <h4>
-            <ChecksumAddress address={this.props.accountAddress} />
-          </h4>
+          <h4>ACCOUNT INFORMATION</h4>
         </header>
 
         <section>
           <dl>
-            <dt>
-              <KeyIcon /> PRIVATE KEY
-            </dt>
-            <dd>{this.props.privateKey}</dd>
+            <dt>ACCOUNT ADDRESS</dt>
+            <dd>
+              <ChecksumAddress address={this.props.accountAddress} />
+            </dd>
+          </dl>
+          <dl>
+            <dt>PRIVATE KEY</dt>
+            <dd>
+              {this.props.privateKey}
+              <br />
+              <p className="danger">
+                Do not use this private key on a public blockchain; use it for
+                development purposes only!
+              </p>
+            </dd>
           </dl>
           <footer>
             <button onClick={this.props.onCloseModal}>DONE</button>
