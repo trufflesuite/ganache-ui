dd570a3bedc813ff67aebe5f95c6018fb0bd00a3
Nicholas J. Paterno <cashlion@protonmail.com>
2019-09-13 09:58:36 -0400

    Major changes to renderer's index.js

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -29,9 +29,40 @@ import LoaderScreen from "./screens/loader/LoaderScreen";
 
 import { ipcRenderer } from "electron";
 
+/* UPDATE HTML FILE */
+
+const htmlStyleTag = document.createElement("style");
+htmlStyleTag.innerHTML = `html {
+  box-sizing: border-box;
+
+  text-shadow: 1px 1px 1px rgba(0,0,0,.004);
+
+  font-smooth: always;
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-family: "Fira Sans", sans-serif;
+  font-weight: 200;
+  background-color: #312c2a;
+}`;
+document.head.appendChild(htmlStyleTag);
+
+
+/* UPDATE HTML FILE */
+
 const store = createStore(RootReducer);
 
 initRenderer(store);
+
+// TODO: see if this is still necessary
+// document.addEventListener('dragover', event => {
+//   event.preventDefault();
+//   return false;
+// });
+// document.addEventListener('drop', event => {
+//   event.preventDefault();
+//   return false;
+// });
 
 // Routes and stylesheets are declared here, rather than in the render
 // function, so that hot module reloading doesn't cause issues with react-router.
@@ -69,76 +100,71 @@ ipcRenderer.on("navigate", (event, path) => {
   hashHistory.push(path);
 });
 
-const stylesheets = [
-  "./styles/colors.scss",
-  "./styles/normalize.scss",
-  "./styles/buttons.scss",
-  "./styles/forms.scss",
-  "./styles/cards.scss",
-  "./app.global.scss",
-  "./components/status-indicator/StatusIndicator.scss",
-  "./components/modal/Modal.scss",
-  "./components/progress-bar/ProgressBar.scss",
-  "./components/spinner/Spinner.css",
-  "./components/file-picker/FilePicker.scss",
-  "./components/styled-select/StyledSelect.scss",
-  "./screens/first-run/FirstRunScreen.scss",
-  "./screens/appshell/AppShell.scss",
-  "./screens/appshell/TopNavbar.scss",
-  "./screens/appshell/BugModal.scss",
-  "./screens/appshell/BugModal.scss",
-  "./screens/accounts/AccountsScreen.scss",
-  "./screens/accounts/AccountList.scss",
-  "./screens/accounts/KeyModal.scss",
-  "./screens/accounts/MnemonicAndHdPath.scss",
-  "./screens/accounts/MnemonicInfoModal.scss",
-  "./screens/blocks/BlocksScreen.scss",
-  "./screens/blocks/BlockList.scss",
-  "./screens/blocks/BlockCard.scss",
-  "./screens/blocks/MiniBlockCard.scss",
-  "./screens/transactions/TransactionsScreen.scss",
-  "./screens/transactions/RecentTransactions.scss",
-  "./screens/transactions/TxList.scss",
-  "./screens/transactions/TxCard.scss",
-  "./screens/transactions/TransactionTypeBadge.scss",
-  "./screens/transactions/MiniTxCard.scss",
-  "./screens/title/TitleScreen.scss",
-  "./screens/auto-update/UpdateModal.scss",
-  "./screens/auto-update/UpdateNotification.scss",
-  "./screens/logs/LogsScreen.scss",
-  "./screens/logs/LogContainer.scss",
-  "./screens/config/ConfigScreen.scss",
-  "./screens/not-found/NotFoundScreen.scss",
-  "./screens/contracts/ContractsScreen.scss",
-  "./screens/contracts/ContractDetailsScreen.scss",
-  "./screens/events/EventList.scss",
-  "./screens/events/EventsScreen.scss",
-  "./screens/events/EventItem.scss",
-  "./screens/event-details/EventDetailsScreen.scss",
-  "./screens/event-details/EncodedEventDetails.scss",
-  "./screens/event-details/DecodedEventDetails.scss",
-  "./screens/startup/HomeScreen.scss",
-  "./screens/loader/LoaderScreen.scss",
-];
+import "./styles/colors.scss";
+import "./styles/normalize.scss";
+import "./styles/buttons.scss";
+import "./styles/forms.scss";
+import "./styles/cards.scss";
+import "./app.global.scss";
+import "./components/status-indicator/StatusIndicator.scss";
+import "./components/modal/Modal.scss";
+import "./components/progress-bar/ProgressBar.scss";
+import "./components/spinner/Spinner.css";
+import "./components/file-picker/FilePicker.scss";
+import "./components/styled-select/StyledSelect.scss";
+import "./screens/first-run/FirstRunScreen.scss";
+import "./screens/appshell/AppShell.scss";
+import "./screens/appshell/TopNavbar.scss";
+import "./screens/appshell/BugModal.scss";
+import "./screens/appshell/BugModal.scss";
+import "./screens/accounts/AccountsScreen.scss";
+import "./screens/accounts/AccountList.scss";
+import "./screens/accounts/KeyModal.scss";
+import "./screens/accounts/MnemonicAndHdPath.scss";
+import "./screens/accounts/MnemonicInfoModal.scss";
+import "./screens/blocks/BlocksScreen.scss";
+import "./screens/blocks/BlockList.scss";
+import "./screens/blocks/BlockCard.scss";
+import "./screens/blocks/MiniBlockCard.scss";
+import "./screens/transactions/TransactionsScreen.scss";
+import "./screens/transactions/RecentTransactions.scss";
+import "./screens/transactions/TxList.scss";
+import "./screens/transactions/TxCard.scss";
+import "./screens/transactions/TransactionTypeBadge.scss";
+import "./screens/transactions/MiniTxCard.scss";
+import "./screens/title/TitleScreen.scss";
+import "./screens/auto-update/UpdateModal.scss";
+import "./screens/auto-update/UpdateNotification.scss";
+import "./screens/logs/LogsScreen.scss";
+import "./screens/logs/LogContainer.scss";
+import "./screens/config/ConfigScreen.scss";
+import "./screens/not-found/NotFoundScreen.scss";
+import "./screens/contracts/ContractsScreen.scss";
+import "./screens/contracts/ContractDetailsScreen.scss";
+import "./screens/events/EventList.scss";
+import "./screens/events/EventsScreen.scss";
+import "./screens/events/EventItem.scss";
+import "./screens/event-details/EventDetailsScreen.scss";
+import "./screens/event-details/EncodedEventDetails.scss";
+import "./screens/event-details/DecodedEventDetails.scss";
+import "./screens/startup/HomeScreen.scss";
+import "./screens/loader/LoaderScreen.scss";
 
 const render = () => {
-  // This is "our hack"; basically, when HMR tells the Javascript
-  // to rerender the whole app, we remove all stylesheets and re-add
-  // them, so they refresh too. Pretty sweet.
-  let links = document.getElementsByTagName("link");
-  links = Array.prototype.slice.call(links);
+  // // This is "our hack"; basically, when HMR tells the Javascript
+  // // to rerender the whole app, we remove all stylesheets and re-add
+  // // them, so they refresh too. Pretty sweet.
+  // let links = document.getElementsByTagName("style");
+  // links = Array.prototype.slice.call(links);
 
-  links.forEach(link => {
-    link.parentElement.removeChild(link);
-  });
-
-  stylesheets.forEach(stylesheet => {
-    var ss = document.createElement("link");
-    ss.type = "text/css";
-    ss.rel = "stylesheet";
-    ss.href = stylesheet;
-    document.getElementsByTagName("head")[0].appendChild(ss);
-  });
+  // links.forEach(link => {
+  //   link.parentElement.removeChild(link);
+  // });
+  //stylesheets.forEach(stylesheet => {
+    // const ss = document.createElement("style");
+    // ss.innerHTML = stylesheets.join("/n");
+    // document.getElementsByTagName("head")[0].appendChild(ss);
+  //});
 
   ReactDOM.render(
     <AppContainer>
@@ -146,12 +172,16 @@ const render = () => {
         <Router history={hashHistory} routes={routes} />
       </Provider>
     </AppContainer>,
-    document.getElementById("root"),
+    document.getElementById("app"),
   );
 };
 
-//store.subscribe(render);
-render();
-if (module.hot) {
-  module.hot.accept(render);
+store.subscribe(render);
+try {
+  render();
+} catch (error) {
+  console.log(error);
 }
+// if (module.hot) {
+//   module.hot.accept(render);
+// }
