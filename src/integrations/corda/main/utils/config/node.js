6bf8c98c2d17cb0d505365b6bb86891f35f84630
Nicholas J. Paterno <cashlion@protonmail.com>
2019-11-01 16:28:26 -0400

    Add permissionsArray to configGen

--- src/integrations/corda/main/utils/config/node.js
+++ src/integrations/corda/main/utils/config/node.js
@@ -1,46 +1,56 @@
 const conf = require("./templates/node.json");
-
+const SPACES = 4;
 const write = console.log;
 
-const format = (el, element) => {
+const format = (el, element, isPermissions) => {
   let result;
-  switch (element.type) {
-    case "string":
-      result = `${el} = "${element.examples[0]}"`;
-      break;
-    default:
-      result = `${el} = ${element.examples[0]}`;
-      break;
+  if (isPermissions) {
+    switch (element.type) {
+      case "string":
+        result = `"${el}"`;
+        break;
+      default:
+        result = `${el}`;
+        break;
+    }
+  } else {
+    switch (element.type) {
+      case "string":
+        result = `${el} = "${element.examples[0]}"`;
+        break;
+      default:
+        result = `${el} = ${element.examples[0]}`;
+        break;
+    }
   }
   return result;
 }
 
-const generate = (root, depth = 0, inline = false) => {
-  const inlineArr = [];
-  const padding = " ".repeat(depth * 2);
-  root.required.map((el) => {
+const generate = (root, depth = 0, isArray = false, isPermissions = false) => {
+  const padding = " ".repeat(depth * SPACES);
+  root.required.forEach((el) => {
     const current = root.properties[el];
-    if (inline) {
-      if (current.type === "array") {
+    if (isArray) {
+      if (current.type === "permissionsArray") {
         // SPECIAL CASE
+        write(`${padding}${el} = [`)
+        generate(current.items, depth + 1, false, true);
+        write(`${padding}]`)
       } else {
-        inlineArr.push(`${el}=${current.examples[0]}`);
+        write(`${padding}${el}=${current.examples[0]}`)    
       }
     } else if (current.type === "object"){
       write(`${padding}${el} {`)
       generate(current, depth + 1);
       write(`${padding}}`)
     } else if (current.type === "array") {
-      write(`${padding}${el} = [`)
+      write(`${padding}${el} = [{`)
       generate(current.items, depth + 1, true);
-      write(`${padding}]`)
+      write(`${padding}}]`)
     } else {
-      write(padding + format(el, current));
+      write(`${padding}${format(el, current, isPermissions)}`);
     }
   })
-  if (inline) {
-    write(`${padding}{ ${inlineArr.join(", ")} }`)    
-  }
 }
 
 generate(conf);
