f31f564e8082c7a922fe2a2079fd0f87201769b7
David Murdoch <david@davidmurdoch.com>
2019-11-05 14:31:35 -0500

    Postgres WIP

---
+++ src/integrations/corda/main/utils/postgres.js
@@ -0,0 +1,50 @@
+const { existsSync } = require("fs");
+const { join } = require("path");
+const { spawnSync } = require("child_process");
+// TODO: this is wrong:
+const postgres_path = "../../../../../integrations/corda/postgres/9.6.15/postgresql-9.6.15-2-linux-x64-binaries/bin";
+
+module.exports = {
+  start: (port, safeWorkspaceName, schemaNames) => {
+    // stop it
+    const dataDir = join(__dirname, `__pg_data_${safeWorkspaceName}`)
+    const exists = existsSync(dataDir);
+
+    if (exists) {
+      try {
+        spawnSync(`${postgres_path}/pg_ctl`, ["-D", dataDir, "stop"]);
+      } catch(e) {
+        // ignore
+      }
+    }
+    if (!exists) {
+      // config it
+      spawnSync(`${postgres_path}/initdb`, ["--pgdata", dataDir, "--username", "postgres"]);
+    }
+
+    // start it
+    // console.log(`"${postgres_path}/pg_ctl" -o "-F -p ${port}" -D "${dataDir}" -l logfile start -w`);
+    spawnSync(`${postgres_path}/pg_ctl`, ["-o", `-F -p ${port}`, "-D", dataDir, "-l", "logfile", "-w", "start"]);
+    try {
+      spawnSync(`${postgres_path}/createuser`, ["--host", "127.0.0.1", "--port", port, "--createdb", "--username", "postgres", "ganache"]);
+      spawnSync(`${postgres_path}/createuser`, ["--host", "127.0.0.1", "--port", port, "--username", "postgres", "corda"]);
+    } catch(e){
+      // ignore
+    }
+
+    for (let i = 0, l = schemaNames.length; i < l; i++) {
+      const schema = schemaNames[i];
+      try {
+        spawnSync(`${postgres_path}/createdb`, ["--host", "127.0.0.1", "--port", port, "--owner", "ganache", "--username", "ganache", schema]);
+      } catch(e) {
+        // ignore
+      }
+    }
+
+    return {
+      stop: () => {
+        return spawnSync(`"${postgres_path}/pg_ctl"`, ["stop" ,"-D", dataDir]);
+      }
+    }
+  }
+};
