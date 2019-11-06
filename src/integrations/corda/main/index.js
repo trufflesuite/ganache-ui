cb44b0bafaabdc9219b585a4272b525e95dc8574
Nicholas J. Paterno <cashlion@protonmail.com>
2019-11-06 16:01:43 -0500

    Update minor config and test utils

--- src/integrations/corda/main/index.js
+++ src/integrations/corda/main/index.js
@@ -1,7 +1,8 @@
 const { NetworkManager } = require("./utils/network");
 
-(async () => {
   const manager = new NetworkManager();
+
+(async () => {
   await manager.bootstrap(2,1);
   console.log("STARTING");
   await manager.start();
--- src/integrations/corda/main/utils/config/config-test.js
+++ src/integrations/corda/main/utils/config/config-test.js
@@ -1,14 +1,15 @@
 const generate = require("./generate");
-// const conf = require("./utils/config/templates/node.json");
+const conf = require("./templates/node.json");
 const { createWriteStream } = require("fs");
-const conf = require("./templates/notary.json");
+// const conf = require("./templates/notary.json");
 const {port} = require("../network");
 
 const stream = createWriteStream("test.conf");
-
+// conf.properties['myLegalName'].value = "DAVID MURDOCH";
 generate(conf, {
     getPort: port(10009),
     write: (val) => stream.write(`${val}\n`, "utf8"),
+    getNonce : ((val) => () => val++)(0),
     postgres: {
         port: 5432,
         schema: "mynode"
