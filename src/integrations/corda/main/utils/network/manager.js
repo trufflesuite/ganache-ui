eae816337b80894362c54bf382009f375b2cfed4
Nicholas J. Paterno <cashlion@protonmail.com>
2019-11-13 15:39:57 -0500

    Update manager to include Braid and Corda modules

--- src/integrations/corda/main/utils/network/manager.js
+++ src/integrations/corda/main/utils/network/manager.js
@@ -1,8 +1,9 @@
-const {writeConfig, bootstrap} = require("./bootstrap");
-const { spawn } = require("child_process");
+const {writeConfig, bootstrap : bootstrapDir} = require("./bootstrap");
 const { EventEmitter } = require("events");
 const { join } = require("path");
 const postgres = require("../postgres");
+const Braid = require("./braid");
+const Corda = require("./corda");
 
 class NetworkManager extends EventEmitter {
   constructor (config, workspaceDirectory) {
@@ -15,63 +16,38 @@ class NetworkManager extends EventEmitter {
   }
 
   async bootstrap(nodes, notaries, port = 10000) {
-    const POSTGRES_HOME = await this.config.corda.files.postgres.download();
+    const BRAID_HOME = this.config.corda.files.braidServer.download();
+    const POSTGRES_HOME = this.config.corda.files.postgres.download();
     const {nodesArr, notariesArr} = await writeConfig(this.workspaceDirectory, nodes, notaries, port);
     this.nodes = nodesArr;
     this.notaries = notariesArr;
     this.entities = this.nodes.concat(this.notaries);
-    this.pg = postgres(POSTGRES_HOME).start(15433, this.workspaceDirectory, this.entities);
-    const output = await bootstrap(this.workspaceDirectory, this.config);
-    
-    this.emit("bootstrap", output);
-    return output;
+    this.pg = postgres(await POSTGRES_HOME).start(15433, this.workspaceDirectory, this.entities);
+    await bootstrapDir(this.workspaceDirectory, this.config);
+    this.braid = new Braid(await BRAID_HOME);
+    // this.emit("bootstrap", output);
+    // return output;
   }
+
+  
 
   async start(){
     const entities = this.entities;
     const JAVA_HOME = await this.config.corda.files.jre.download();
+    const config = { env : { PATH : `${JAVA_HOME}/bin:$PATH` } };
 
     const promises = entities.map((entity) => {
       const currentPath = join(this.workspaceDirectory, entity);
-      const java = spawn("java", ["-jar", "corda.jar"], {
-        cwd : currentPath,
-        env : {
-          PATH : `${JAVA_HOME}/bin:$PATH`
-        }
-      });
-
-      java.stderr.on('data', (data) => {
-        console.error(`stderr:\n${data}`);
-      });
-
-      java.on("error", console.error);
-
-      return new Promise((resolve, reject) => {
-        java.once('close', (code) => {
-          // TODO: handle premature individual node shutdown
-          /// close postgres, close other nodes, what do?
-          console.log(`child process exited with code ${code}`);
-          reject();
-        });
-
-        java.stdout.on('data', (data) => {
-          console.log(`${data}`);
-          if (data.toString().includes('" started up and registered in ')) {
-            resolve({
-              process: java,
-              entity
-            });
-          }
-        });
-      });
+      const currentConfig = Object.assign({ cwd : currentPath }, config);
+      this.braid.start(entity, currentPath);
+      const corda = new Corda(entity, currentConfig);
+      this.processes.push(corda);
+      return corda.start();
     });
-    return Promise.all(promises).then((entities) => {
-      entities.forEach(({process}) => {
-        this.processes.push(process);
-      })
-      return entities;
-    });
+    this.braid.getAll();
+    return Promise.all(promises);
   }
+
   stop() {
     try {
       this.pg && this.pg.stop();
@@ -80,17 +56,7 @@ class NetworkManager extends EventEmitter {
       console.error("Error occured while attempting to stop postgres...");
       console.error(e);
     }
-    let process;
-    let promises = [];
-    while (process = this.processes.shift()) {
-      promises.push(new Promise(resolve => {
-        process.once("close", resolve);
-      }));
-      if (!process.killed) {
-        process.kill();
-      }
-    }
-    return Promise.all(promises);
+    return Promise.all(this.processes.map(cordaProcess => cordaProcess.stop()))
   }
   
   getNodes(){
