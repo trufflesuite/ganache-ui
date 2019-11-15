4379979ded95b40f956748309921e68222f1d725
David Murdoch <david@davidmurdoch.com>
2019-11-15 12:08:40 -0500

    Create complete corda quickstart config

--- src/main/types/settings/flavors/corda.js
+++ src/main/types/settings/flavors/corda.js
@@ -1,6 +1,38 @@
 module.exports = {
   name: "Quickstart",
   isDefault: true,
-  nodes: ["PartyA", "PartyB"],
-  notaries: ["Notary"]
+  nodes: [{
+    "name": "O=Party A,L=London,C=GB",
+    "safeName" : "partya",
+    "rpcPort": 10000,
+    "adminPort": 10001,
+    "p2pPort": 10002,
+    "dbPort": 5432,
+    "cordapps": []
+  },{
+    "name": "O=Party B,L=Paris,C=FR",
+    "safeName" : "partyb",
+    "rpcPort": 10003,
+    "adminPort": 10004,
+    "p2pPort": 10005,
+    "dbPort": 5432,
+    "cordapps": []
+  },{
+    "name": "O=Party C,L=New York,C=US",
+    "safeName" : "partyc",
+    "rpcPort": 10006,
+    "adminPort": 10007,
+    "p2pPort": 10008,
+    "dbPort": 5432,
+    "cordapps": []
+  }],
+  notaries : [{
+    "name": "O=Notary Service,L=London,C=GB",
+    "safeName" : "notary",
+    "rpcPort": 10009,
+    "adminPort": 10010,
+    "p2pPort": 10011,
+    "dbPort": 5432,
+    "cordapps": []
+  }]
 };
