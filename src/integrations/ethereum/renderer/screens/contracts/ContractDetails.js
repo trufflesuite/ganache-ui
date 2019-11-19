1eb503c7c2bf9f5f60c8e7e626b3c4970cba93ec
David Murdoch <david@davidmurdoch.com>
2019-11-19 16:04:31 -0500

    Move JSON theme for viewing state to its own file

--- src/integrations/ethereum/renderer/screens/contracts/ContractDetails.js
+++ src/integrations/ethereum/renderer/screens/contracts/ContractDetails.js
@@ -1,6 +1,7 @@
 import { shell } from "electron";
 const { app } = require("electron").remote;
 
+import jsonTheme from "../../../../../common/utils/jsonTheme";
 import ReactJson from "@seesemichaelj/react-json-view";
 import React, { Component } from "react";
 import connect from "../../../../../renderer/screens/helpers/connect";
@@ -152,28 +153,7 @@ class ContractDetails extends Component {
                   this.props.workspaces.current.shownContract.state.variables
                 }
                 name={false}
-                theme={{
-                  scheme: "ganache",
-                  author: "Ganache",
-                  //transparent main background
-                  base00: "rgba(0, 0, 0, 0)",
-                  base01: "rgb(245, 245, 245)",
-                  base02: "#000", // lines and background color for: NULL, undefined, and brackets
-                  base03: "rgb(26, 185, 70)", // blue grey -- not used?
-                  base04: "rgba(0, 0, 0, 0.3)",
-                  base05: "#aaa", // undefined text
-                  base06: "#073642", // dark blue -- not sued?
-                  base07: "#000", // JSON keys
-                  base08: "#d33682", // pink -- not used?
-                  base09: "rgb(208, 108, 0)", // string types text (ganache orange)
-                  base0A: "rgb(208, 108, 0)", // NULL (ganache orange)
-                  base0B: "#3fe0c5", //aka --truffle-green, for  float types
-                  base0C: "#777", // array indexes and item counts
-                  base0D: "#000", // arrows
-                  base0E: "#000", // used for some arrows and bool
-                  base0F: "#268bd2", // a bright blue -- not used?
-                  base10: "rgb(26, 185, 70)", // a bright blue -- not used?
-                }}
+                theme={jsonTheme}
                 iconStyle="triangle"
                 edit={false}
                 add={false}
