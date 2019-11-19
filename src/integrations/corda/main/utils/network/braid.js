b3e774fe25d229e7c55fe01fc1c8f014e147fdcb
David Murdoch <david@davidmurdoch.com>
2019-11-19 16:06:17 -0500

    Remove braid's the unused port const

--- src/integrations/corda/main/utils/network/braid.js
+++ src/integrations/corda/main/utils/network/braid.js
@@ -3,8 +3,6 @@ const { join } = require("path");
 
 // java -jar braid-server.jar localhost:10007 user1 letmein 8000 3 ./corda/party1/cordapps/
 
-let port = 20000; // TODO: change this
-
 class Braid {
   constructor(braid_home){
     this.BRAID_HOME = braid_home;
@@ -15,7 +13,7 @@ class Braid {
     const name = entity.safeName;
     const exists = this.servers.get(name);
     if (!exists) {
-      const args = ["-jar", "braid-server.jar", `localhost:${entity.rpcPort}`, "user1", "letmein", port++, 3, ...entity.cordapps];
+      const args = ["-jar", "braid-server.jar", `localhost:${entity.rpcPort}`, "user1", "letmein", entity.rpcPort + 10000, 3, ...entity.cordapps];
       // current env PATH seems to be required for braid to actually work
       const braid = spawn(join(JAVA_HOME, "bin", "java"), args, { cwd : this.BRAID_HOME, env: { PATH: process.env.PATH } });
 
