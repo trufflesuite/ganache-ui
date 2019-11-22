cea4bf9f084d1d160fe96e77771f5186171af2d2
David Murdoch <david@davidmurdoch.com>
2019-11-22 17:36:29 -0500

    Debounce loader progress updates

--- src/renderer/screens/loader/LoaderScreen.js
+++ src/renderer/screens/loader/LoaderScreen.js
@@ -7,6 +7,51 @@ import BugModal from "../appshell/BugModal";
 import Logo from "../../components/logo/Logo";
 
 class LoaderScreen extends Component {
+  
+  constructor(){
+    super();
+    this.state = {progress: this.props && this.props.core ? this.props.core.progress : "", lastProgressUpdateTime: 0};
+    this.queued = [];
+    this.timer = null;
+    this.minTimeOnprogressUpdate = 1000;
+  }
+  progressIsReadyForUpdate(now) {
+    return this.queued.length === 0 && (this.state.lastProgressUpdateTime == 0 || ((this.state.lastProgressUpdateTime + this.minTimeOnprogressUpdate) <= now));
+  }
+  componentWillReceiveProps(nextProps) {
+    const currentProgress = this.state.progress;
+    if (nextProps.core.progress !== currentProgress) {
+      const now = Date.now();
+      if (this.progressIsReadyForUpdate(now)) {
+        this.setState({ progress: nextProps.core.progress, lastProgressUpdateTime: now });
+      } else {
+        const length = this.queued.push(nextProps.core.progress);
+
+        // if there was already something in the queue we don't need to setTimeout here.
+        if (length === 1) {
+          const waitFor = Math.min(now - this.state.lastProgressUpdateTime, this.minTimeOnprogressUpdate);
+          this.timer = setTimeout(function update() {
+            let progress;
+            do {
+              progress = this.queued.shift();
+            } while(progress === this.state.progress && this.queued.length > 0);
+
+            if (progress !== this.state.progress) {
+              const now = Date.now();
+              this.setState({ progress, lastProgressUpdateTime:  now}, () => {
+                if (this.queued.length > 0) {
+                  this.timer = setTimeout(update.bind(this), this.minTimeOnprogressUpdate);
+                }
+              });
+            }
+          }.bind(this), waitFor);
+        }
+      }
+    }
+  }
+  componentWillUnmount(){
+    clearTimeout(this.timer);
+  }
   render() {
     return (
       <div className="LoaderScreenContainer">
@@ -19,7 +64,7 @@ class LoaderScreen extends Component {
             duration={2666}
           />
           <Logo fadeInElement={true} />
-          <h4 className="LoaderProgress">{this.props.core.progress}</h4>
+          <h4 className="LoaderProgress">{this.state.progress}</h4>
         </div>
         <OnlyIf
           test={
