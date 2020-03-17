73a5df8abf3d8378cbaac2ba69768b57147d14a9
David Murdoch <david@davidmurdoch.com>
2020-03-17 12:31:12 -0400

    Make sure ethereum projects are set by not overwriting them with the default empty list

--- src/main/index.js
+++ src/main/index.js
@@ -329,11 +329,6 @@ app.on('ready', () => {
         const globalSettings = global.getAll();
         const workspaceSettings = workspace.settings.getAll();
         mainWindow.webContents.send(
-          SET_CURRENT_WORKSPACE,
-          workspace,
-          workspace.contractCache.getAll(),
-        );
-        mainWindow.webContents.send(
           SET_SERVER_STARTED,
           globalSettings,
           workspaceSettings,
