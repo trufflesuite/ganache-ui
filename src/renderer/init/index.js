a6c5119656011f330e638d0d11eed28a8bc3f254
Nicholas J. Paterno <cashlion@protonmail.com>
2020-03-25 14:47:01 -0400

    Add corda core init to renderer process

--- src/renderer/init/index.js
+++ src/renderer/init/index.js
@@ -1,6 +1,7 @@
 import { initAutoUpdates } from "./AutoUpdate";
 import { initCore } from "./Core";
 import { initConfig } from "./Config";
+import { initCordaCore } from "./CordaCore";
 import { initCorda } from "./CordaShell";
 import { initLogs } from "./Logs";
 import { initNetwork } from "./Network";
@@ -19,6 +20,7 @@ export function initRenderer(store) {
   initAutoUpdates(store);
   initWorkspaces(store);
   initEvents(store);
+  initCordaCore(store);
   initCorda(store);
   initCordaShutdown(store);
 }
