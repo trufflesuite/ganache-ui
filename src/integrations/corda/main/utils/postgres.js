3fb30d8cbb58dba00820de60d4acf525be4ee2cf
David Murdoch <david@davidmurdoch.com>
2020-03-30 19:15:46 -0400

    Start up postgres with more connections, if needed

--- src/integrations/corda/main/utils/postgres.js
+++ src/integrations/corda/main/utils/postgres.js
@@ -70,13 +70,14 @@ module.exports = (POSTGRES_PATH) => {
         // initialize a postgres database
         await spawn(INITDB, ["--pgdata", dataDir, "--username", "postgres"], config);
       }
-
+      
       // make sure there are no databases of ours running on this port...
       await postgres.stop(port);
-
+      
       // TODO: figure out what postgres needs on Windows from the process.env and only supply that.
       // start the postgres server
-      await spawn(PG_CTL, ["-o", `-F -p ${port}`, "-D", dataDir, "-l", join(dataDir, "postgres-logfile.log"), "-w", "start"]);
+      const maxConnections = Math.min(Math.max(100, schemaNames.length * 10), 1000);
+      await spawn(PG_CTL, ["-o", `-F -p ${port} -N ${maxConnections}`, "-D", dataDir, "-l", join(dataDir, "postgres-logfile.log"), "-w", "start"]);
       
       try {
         await initSchema(port, schemaNames);
