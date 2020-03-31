817e51490cf9082470fe4b76818f441fcc10583b
Nicholas J. Paterno <cashlion@protonmail.com>
2020-03-31 17:59:35 -0400

    Add corda core actions

--- src/common/redux/corda-core/actions.js
+++ src/common/redux/corda-core/actions.js
@@ -1,6 +1,18 @@
-const prefix = "CORDA_CORE"; 
+const prefix = "CORDA_CORE";
+import { setToast } from "../../../common/redux/network/actions";
+import { requestServerRestart } from "../../../common/redux/core/actions.js";
 
 export const REFRESH_CORDAPP = `${prefix}/REFRESH_CORDAPP`;
-export const refreshCordapp = function(cordapps) {
+export const sendRefreshCordapp = function(cordapps) {
   return { type: REFRESH_CORDAPP, cordapps };
 };
+
+
+export const refreshCordapp = function(cordapps) {
+  return function(dispatch) {
+    dispatch(setToast(cordapps.join(', ') + " has changed. Restart?", true, "Restart", () => {
+      dispatch(requestServerRestart());
+    }));
+    return ({ type: REFRESH_CORDAPP, cordapps });
+  };
+}
--- src/renderer/init/CordaCore.js
+++ src/renderer/init/CordaCore.js
@@ -1,8 +1,8 @@
 import { ipcRenderer } from "electron";
-import { REFRESH_CORDAPP } from "../../common/redux/corda-core/actions";
+import { REFRESH_CORDAPP, refreshCordapp } from "../../common/redux/corda-core/actions";
 
 export function initCordaCore(store) {
   ipcRenderer.on(REFRESH_CORDAPP, (_event, data) => {
-    store.dispatch(data);
+    store.dispatch(refreshCordapp(data));
   });
 }
