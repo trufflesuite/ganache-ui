6ae629f0dc3daddc65e3fac81a1bb865a138288c
David Murdoch <davidmurdoch@users.noreply.github.com>
2020-04-14 18:03:30 -0400

    feat: quickstart quick save (#1716)
    * Don't stop/start during quickstart "Save" (WIP)
    
    * Ensure quickstart settings persist on save (WIP)
    
    * Make sure we don't overwrite the quickstart randomizeMnemonicOnStart setting

--- src/main/index.js
+++ src/main/index.js
@@ -4,6 +4,7 @@ const { spawn } = require("promisify-child-process");
 import path from "path";
 import * as os from "os";
 import merge from "lodash.merge";
+import clonedeep from "lodash.clonedeep";
 import ethagen from "ethagen/wallet";
 import moniker from "moniker";
 import fixPath from "fix-path"
@@ -452,6 +453,21 @@ app.on('ready', () => {
       // the projects should trigger the REQUEST_SERVER_RESTART
       // logic
       workspace.settings.set("projects", []);
+      const randomizeMnemonicOnStart = workspace.settings.get("randomizeMnemonicOnStart");
+      workspace.saveAs(
+        "Quickstart",
+        null,
+        workspaceManager.directory,
+        null,
+        true,
+      );
+      // this loads the default workspace
+      workspaceManager.bootstrap();
+      // saveAs overwrites "isDefault", so we need to put it back
+      workspace.settings.set("isDefault", true);
+      workspace.settings.set("randomizeMnemonicOnStart", randomizeMnemonicOnStart);
+      await integrations.setWorkspace("Quickstart", flavor);
+      workspace = integrations.workspace;
     } else {
       for (let i = 0; i < workspaceSettings.projects.length; i++) {
         projects.push(
@@ -510,6 +526,7 @@ app.on('ready', () => {
         null,
         workspaceManager.directory,
         wallet.mnemonic,
+        true,
       );
     } else {
       const workspaceSettings = defaultWorkspace.settings.getAll();
@@ -634,6 +651,18 @@ app.on('ready', () => {
       global.setAll(globalSettings);
 
       if (workspace && workspaceSettings) {
+        // if the current workspace is a "default workspace", make sure
+        // we update the original default workspace as well!
+        if (workspace.settings.get("isDefault")) {
+          const defaultWorkspace = workspaceManager.get(null, workspace.flavor);
+          const clonedSettings = clonedeep(workspaceSettings);
+          if (clonedSettings.server && clonedSettings.server.db_path) {
+            clonedSettings.server.db_path = defaultWorkspace.settings.get("server.db_path");
+          }
+          defaultWorkspace.settings.setAll(clonedSettings);
+          workspace.resetChaindata();
+        }
+        workspaceSettings.randomizeMnemonicOnStart = false;
         workspace.settings.setAll(workspaceSettings);
       }
 
