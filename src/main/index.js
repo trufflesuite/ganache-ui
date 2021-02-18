trufflesuite/ganache-ui

commit 6ae629f0dc3daddc65e3fac81a1bb865a138288c

feat: quickstart quick save (#1716)
* Don't stop/start during quickstart "Save" (WIP)

* Ensure quickstart settings persist on save (WIP)

* Make sure we don't overwrite the quickstart randomizeMnemonicOnStart setting

src/main/index.js
@@ -4,6 +4,7 @@ const { spawn } = require("promisify-child-process");
 import path from "path";
 import * as os from "os";
 import merge from "lodash.merge";
+import clonedeep from "lodash.clonedeep";
 import ethagen from "ethagen/wallet";
 import moniker from "moniker";
 import fixPath from "fix-path"
@@ -452,6 +453,21 @@ app.on('ready', () => {
       // the projects should trigger the REQUEST_SERVER_RESTART
       // logic
       workspace.settings.set("projects", []);
+      const randomizeMnemonicOnStart = workspace.settings.get("randomizeMnemonicOnStart");
+      workspace.saveAs(
+        "Quickstart",
+        null,
+        workspaceManager.directory,
+        null,
+        true,
+      );
+      // this loads the default workspace
+      workspaceManager.bootstrap();
+      // saveAs overwrites "isDefault", so we need to put it back
+      workspace.settings.set("isDefault", true);
+      workspace.settings.set("randomizeMnemonicOnStart", randomizeMnemonicOnStart);
+      await integrations.setWorkspace("Quickstart", flavor);
+      workspace = integrations.workspace;
     } else {
       for (let i = 0; i < workspaceSettings.projects.length; i++) {
         projects.push(
@@ -510,6 +526,7 @@ app.on('ready', () => {
         null,
         workspaceManager.directory,
         wallet.mnemonic,
+        true,
       );
     } else {
       const workspaceSettings = defaultWorkspace.settings.getAll();
@@ -634,6 +651,18 @@ app.on('ready', () => {
       global.setAll(globalSettings);
 
       if (workspace && workspaceSettings) {
+        // if the current workspace is a "default workspace", make sure
+        // we update the original default workspace as well!
+        if (workspace.settings.get("isDefault")) {
+          const defaultWorkspace = workspaceManager.get(null, workspace.flavor);
+          const clonedSettings = clonedeep(workspaceSettings);
+          if (clonedSettings.server && clonedSettings.server.db_path) {
+            clonedSettings.server.db_path = defaultWorkspace.settings.get("server.db_path");
+          }
+          defaultWorkspace.settings.setAll(clonedSettings);
+          workspace.resetChaindata();
+        }
+        workspaceSettings.randomizeMnemonicOnStart = false;
         workspace.settings.setAll(workspaceSettings);
       }
 

src/integrations/index.js
@@ -66,8 +66,6 @@ class IntegrationManager extends EventEmitter {
     } else {
       workspace = this.workspaceManager.get(null);
     }
-    
-    await this.flavor.stopServer();
 
     workspace.saveAs(
       workspaceName,
@@ -76,11 +74,9 @@ class IntegrationManager extends EventEmitter {
       mnemonic
     );
 
-    await this.workspaceManager.bootstrap();
-
     await this.setWorkspace(workspaceName, workspace.flavor);
 
-    await this.startServer();
+    this.emit("server-started");
   }
 
   async startChain() {

src/common/redux/workspaces/actions.js
@@ -71,8 +71,6 @@ export const saveWorkspace = function(name) {
   return function(dispatch, getState) {
     const mnemonic = getState().core.mnemonic;
 
-    cleanupWorkspace(dispatch, getState);
-
     dispatch({ type: SAVE_WORKSPACE, name, mnemonic: mnemonic });
 
     ipcRenderer.send(SAVE_WORKSPACE, name, mnemonic);

src/main/types/workspaces/Workspace.js
@@ -1,6 +1,5 @@
 import path from "path";
 import fse from "fs-extra";
-import temp from "temp";
 
 import WorkspaceSettings from "../settings/WorkspaceSettings";
 import ContractCache from "../../../integrations/ethereum/main/types/contracts/ContractCache";
@@ -60,11 +59,7 @@ class Workspace {
   }
 
   generateChaindataDirectory() {
-    if (this.name === null) {
-      return temp.path();
-    } else {
-      return path.join(this.workspaceDirectory, "chaindata");
-    }
+    return path.join(this.workspaceDirectory, "chaindata");
   }
 
   // creates the directory if needed (recursively)
@@ -83,12 +78,30 @@ class Workspace {
     this.settings.bootstrap();
   }
 
-  saveAs(name, chaindataDirectory, configDirectory, mnemonic) {
+  saveAs(name, chaindataDirectory, configDirectory, mnemonic, updateLocations) {
     this.name = name;
-    this.init(configDirectory);
-    this.bootstrapDirectory();
+    
+    if (updateLocations) {
+      this.init(configDirectory);
+      this.bootstrapDirectory();
+      this.settings.setDirectory(this.workspaceDirectory);
 
-    this.settings.setDirectory(this.workspaceDirectory);
+      // make sure contractCache is in the right location:
+      if (chaindataDirectory && chaindataDirectory !== this.chaindataDirectory) {
+        fse.copySync(chaindataDirectory, this.chaindataDirectory);
+      }
+      const currentContractCachePath = path.join(this.contractCache.storage.directory, this.contractCache.storage.name);
+      const desiredContractCachePath = path.join(this.chaindataDirectory, this.contractCache.storage.name);
+      if (currentContractCachePath !== desiredContractCachePath && fse.existsSync(currentContractCachePath)) {
+        fse.copySync(currentContractCachePath, desiredContractCachePath);
+      }
+      
+      this.contractCache.setDirectory(this.chaindataDirectory);
+      this.contractCache.setAll(this.contractCache.getAll());
+    }
+    
+    this.settings.chaindataDirectory = this.chaindataDirectory;
+    this.settings.set("server.db_path", this.chaindataDirectory);
     this.settings.set("name", name);
     this.settings.set("isDefault", false);
     if (this.flavor === "corda") {
@@ -96,19 +109,6 @@ class Workspace {
     }
     this.settings.set("randomizeMnemonicOnStart", false);
     this.settings.set("server.mnemonic", mnemonic);
-
-    // make sure contractCache is in the right location:
-    if (chaindataDirectory && chaindataDirectory !== this.chaindataDirectory) {
-      fse.copySync(chaindataDirectory, this.chaindataDirectory);
-    }
-    const currentContractCachePath = path.join(this.contractCache.storage.directory, this.contractCache.storage.name);
-    const desiredContractCachePath = path.join(this.chaindataDirectory, this.contractCache.storage.name);
-    if (currentContractCachePath !== desiredContractCachePath && fse.existsSync(currentContractCachePath)) {
-      fse.copySync(currentContractCachePath, desiredContractCachePath);
-    }
-
-    this.contractCache.setDirectory(this.chaindataDirectory);
-    this.contractCache.setAll(this.contractCache.getAll());
   }
 
   delete() {

src/main/types/workspaces/WorkspaceManager.js
@@ -14,16 +14,24 @@ class WorkspaceManager {
     if (fse.existsSync(workspacesDirectory)) {
       this.workspaces = fse
         .readdirSync(workspacesDirectory)
-        .filter(file => {
-          return fse.lstatSync(path.join(workspacesDirectory, file))
-            .isDirectory;
-        })
-        .map(file => {
+        .flatMap(file => {
+          if (!fse.lstatSync(path.join(workspacesDirectory, file)).isDirectory) {
+            return [];
+          }
           let settings = new WorkspaceSettings(
             path.join(workspacesDirectory, file),
             path.join(workspacesDirectory, file, "chaindata"),
           );
+          
+          const isQuickstart = settings.get("isDefault");
+          if (isQuickstart) {
+            // the default workspace shouldn't be in the "workspaces" directory,
+            // delete it.
+            fse.remove(settings.settings.directory).catch(e => e);
+            return [];
+          }
           settings.bootstrap();
+
           const name = settings.get("name");
           const sanitizedName = Workspace.getSanitizedName(name);
           if (sanitizedName !== file) {
@@ -41,7 +49,7 @@ class WorkspaceManager {
             }
           }
           const flavor = settings.get("flavor");
-          return new Workspace(name, this.directory, flavor);
+          return [new Workspace(name, this.directory, flavor)];
         });
     }
 

src/renderer/screens/appshell/TopNavbar.js
@@ -207,7 +207,7 @@ class TopNavbar extends Component {
               title="WORKSPACE"
               value={this.props.config.settings.workspace.name}
             />
-            <OnlyIf test={this.props.workspaces.current.name === null}>
+            <OnlyIf test={this.props.config.settings.workspace.isDefault}>
               <button onClick={this.handleSaveWorkspacePress.bind(this)}>Save</button>
             </OnlyIf>
             <button onClick={this.handleWorkspacesPress.bind(this)}>Switch</button>

commit 3de6a6e5d8c539d2728b5a7bb3b0c0c2f55668dc

fix default workspace saving (fixes bug introduced in #1716)

src/main/index.js
@@ -4,7 +4,6 @@ const { spawn } = require("promisify-child-process");
 import path from "path";
 import * as os from "os";
 import merge from "lodash.merge";
-import clonedeep from "lodash.clonedeep";
 import ethagen from "ethagen/wallet";
 import moniker from "moniker";
 import fixPath from "fix-path"
@@ -448,21 +447,6 @@ app.on('ready', async () => {
       // the projects should trigger the REQUEST_SERVER_RESTART
       // logic
       workspace.settings.set("projects", []);
-      const randomizeMnemonicOnStart = workspace.settings.get("randomizeMnemonicOnStart");
-      workspace.saveAs(
-        "Quickstart",
-        null,
-        workspaceManager.directory,
-        null,
-        true,
-      );
-      // this loads the default workspace
-      workspaceManager.bootstrap();
-      // saveAs overwrites "isDefault", so we need to put it back
-      workspace.settings.set("isDefault", true);
-      workspace.settings.set("randomizeMnemonicOnStart", randomizeMnemonicOnStart);
-      await integrations.setWorkspace("Quickstart", flavor);
-      workspace = integrations.workspace;
     } else {
       for (let i = 0; i < workspaceSettings.projects.length; i++) {
         projects.push(
@@ -646,19 +630,10 @@ app.on('ready', async () => {
       global.setAll(globalSettings);
 
       if (workspace && workspaceSettings) {
-        // if the current workspace is a "default workspace", make sure
-        // we update the original default workspace as well!
+        workspace.settings.setAll(workspaceSettings);
         if (workspace.settings.get("isDefault")) {
-          const defaultWorkspace = workspaceManager.get(null, workspace.flavor);
-          const clonedSettings = clonedeep(workspaceSettings);
-          if (clonedSettings.server && clonedSettings.server.db_path) {
-            clonedSettings.server.db_path = defaultWorkspace.settings.get("server.db_path");
-          }
-          defaultWorkspace.settings.setAll(clonedSettings);
           integrations.stopServer().then(() => workspace.resetChaindata())
         }
-        workspaceSettings.randomizeMnemonicOnStart = false;
-        workspace.settings.setAll(workspaceSettings);
       }
 
       GoogleAnalytics.reportWorkspaceSettings(workspaceSettings);

src/renderer/screens/config/ConfigScreen.js
@@ -28,6 +28,7 @@ import CordaAdvancedScreen from "../../../integrations/corda/renderer/screens/co
 import RestartIcon from "../../icons/restart.svg";
 import EjectIcon from "../../icons/eject.svg";
 import SaveIcon from "../../icons/save-icon.svg";
+import StartIcon from "../../icons/start.svg";
 import OnlyIf from "../../components/only-if/OnlyIf";
 
 const ETHEREUM_TABS = [
@@ -101,7 +102,7 @@ class ConfigScreen extends PureComponent {
     }
   };
 
-  restartServer = () => {
+  saveWorkspace = () => {
     this.props.dispatch(Config.clearAllSettingErrors());
     // eslint-disable-next-line react/no-direct-mutation-state
     this.state.config.validationErrors = {};
@@ -118,7 +119,12 @@ class ConfigScreen extends PureComponent {
         ),
       );
     }
-    this.props.dispatch(Core.requestServerRestart());
+
+    if (this.props.config.startupMode !== Config.STARTUP_MODE.EDIT_WORKSPACE) {
+      this.props.dispatch(Core.requestServerRestart());
+    } else {
+      this.props.dispatch(Core.showHomeScreen());
+    }
   };
 
   isDirty() {
@@ -396,16 +402,24 @@ class ConfigScreen extends PureComponent {
               </button>
               <button
                 className="btn btn-primary"
-                onClick={this.restartServer}
+                onClick={this.saveWorkspace}
                 disabled={this.invalidConfig()}
               >
                 <OnlyIf
                   test={
-                    this.props.config.startupMode !== Config.STARTUP_MODE.NORMAL
+                    this.props.config.startupMode === Config.STARTUP_MODE.EDIT_WORKSPACE
                   }
                 >
                   <SaveIcon className="save-icon" /*size={18}*/ />
                   SAVE WORKSPACE
+                </OnlyIf>
+                <OnlyIf
+                  test={
+                    this.props.config.startupMode === Config.STARTUP_MODE.NEW_WORKSPACE
+                  }
+                >
+                  <StartIcon className="start-icon" /*size={18}*/ />
+                  START
                 </OnlyIf>
                 <OnlyIf
                   test={