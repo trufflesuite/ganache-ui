adc8c5779e5d6357bb486407806e0c0e76693f02
Mike Seese <seesemichaelj@gmail.com>
2021-03-09 15:03:41 -0800

    fix tipsetlist loading new tipsets

--- src/integrations/filecoin/renderer/screens/tipsets/TipsetList.js
+++ src/integrations/filecoin/renderer/screens/tipsets/TipsetList.js
@@ -27,19 +27,23 @@ class TipsetList extends Component {
       return;
     }
 
-    // No change in scroll position? If a new block has been added,
-    // request the previous page
-    if (prevProps.tipsets.inView.length == 0) {
-      return;
-    }
-
-    const latestTipsetInView = prevProps.tipsets.inView[0].Height;
-    if (
-      prevProps.appshell.scrollPosition == "top" &&
-      prevProps.core.latestTipset > latestTipsetInView &&
-      !latestRequested
-    ) {
-      this.props.dispatch(Tipsets.requestPreviousPage());
+    // No change in scroll position? If a new tipset has been added,
+    // request the previous page. Be sure not to try to rerequest
+    // if we already requested pages
+    if (!latestRequested) {
+      if (prevProps.tipsets.inView.length === 0) {
+        if (this.props.core.latestTipset > 0) {
+          this.props.dispatch(Tipsets.requestPreviousPage());
+        }
+      } else {
+        const latestTipsetInView = prevProps.tipsets.inView[0].Height;
+        if (
+          prevProps.appshell.scrollPosition === "top" &&
+          this.props.core.latestTipset > latestTipsetInView
+        ) {
+          this.props.dispatch(Tipsets.requestPreviousPage());
+        }
+      }
     }
   }
 
