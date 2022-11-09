add00a566ec761b6d7d0c4a5248bdf197e9b9e8d
Jeff Smale <6363749+jeffsmale90@users.noreply.github.com>
2022-11-09 21:59:16 +0100

    Fix logs screen

--- src/renderer/screens/logs/LogsScreen.js
+++ src/renderer/screens/logs/LogsScreen.js
@@ -1,22 +1,37 @@
 import React, { Component } from "react";
-
-import LogContainer from "./LogContainer";
+import connect from "../helpers/connect";
 import LogContainerLazy from "./LogContainerLazy";
-
-const isLazy = true;
+import { CellMeasurerCache } from "react-virtualized";
 
 class Logs extends Component {
   constructor() {
     super();
   }
 
+  caches = new Map();
+
   render() {
+    const context = this.props.match.params.context || "default";
+
+    let logCache;
+    if (this.caches.has(context)) {
+      logCache = this.caches.get(context);
+    } else {
+      logCache = new CellMeasurerCache({
+        defaultHeight: 50,
+        fixedWidth: true,
+      });
+      this.caches.set(context, logCache);
+    }
+
     return (
       <div className="LogsScreen">
-        <main>{isLazy ? <LogContainerLazy /> : <LogContainer />}</main>
+        <main>
+          <LogContainerLazy cache={logCache} context={context} />
+        </main>
       </div>
     );
   }
 }
 
-export default Logs;
+export default connect(Logs, "config");
